61d269f75c01c80a0660e877b1f506b3b312d632
peterliao2004 <peterliao2004@gmail.com>
2026-05-12 13:26:59 +1000

    Kind of working

--- pibot/STOP.py
+++ pibot/STOP.py
@@ -1,4 +1,4 @@
 from pibot_client import PiBot
-bot = PiBot(ip="172.19.232.145")
+bot = PiBot(ip="172.19.232.150")
 
 bot.setVelocity(0,0,2)
--- pibot/go_to_goal.py
+++ pibot/go_to_goal.py
@@ -3,7 +3,7 @@ from pibot_client import PiBot
 import math
 import time
 import cv2
-from utility_functions import wrap_to_180
+from utility_functions import wrap_to_pi
 bot = PiBot(ip="172.19.232.150", localiser_ip='egb439localiser2')
 
 # Goal posisition
@@ -14,46 +14,54 @@ acceptable_diff = 0.1
 # Proportional gain for angular velocity
 k = 10
 
+max_speed = 50
+
 W = 0.145 #width between wheels
 R = 0.065 #radius of wheel
 
-v = 10 #target velocity
+v_linear = 50 #target linear velocity
 
 while(1):
     # Gets the localiser pose, which is a list of [x, y, theta]
     pose = bot.getLocalizerPose()
-    
-    ## Stop condition
-    # if (pose[0] > (goal[0] - acceptable_diff) or pose[0] < (goal[0] + acceptable_diff)) and (pose[1] > (goal[1] - acceptable_diff) or pose[1] < (goal[1] + acceptable_diff)):
-    #     bot.setVelocity(0,0)
-    #     break
     
     # If the localiser fails to give a pose, skip this loop and try again
     if pose is None:
         print("Missed pose step\n")
         time.sleep(0.2)
         continue
+    
+    ## Stop condition
+    distance_to_goal = math.sqrt((goal[0] - pose[0])**2 + (goal[1] - pose[1])**2)
+    
+    if distance_to_goal < acceptable_diff:
+        bot.setVelocity(0, 0)
+        print("Goal reached")
+        break
 
-    # Calculate the target angle to the goal (in degrees)
-    theta_targ = math.degrees(math.atan2((goal[1] - pose[1]), (goal[0] - pose[0])))
+    # Calculate the target angle to the goal (in radians)
+    theta_targ = math.atan2((goal[1] - pose[1]), (goal[0] - pose[0]))
     
     # Calculate the difference between the target angle and the current angle
-    theta_diff = wrap_to_180(theta_targ - pose[2])
+    theta_diff = wrap_to_pi(theta_targ - math.radians(pose[2]))
     
     # Calculate the target angular velocity using a proportional controller
     omega_targ = k * theta_diff
-    
+
     # Calculate the wheel velocities needed to achieve the target velocity
-    theta_dot_R = v + omega_targ
-    theta_dot_L = v - omega_targ
+    theta_dot_R = v_linear + omega_targ
+    theta_dot_L = v_linear - omega_targ
+    # Cap wheel velocity
+    theta_dot_R = max(min(theta_dot_R, max_speed), -max_speed)
+    theta_dot_L = max(min(theta_dot_L, max_speed), -max_speed)
     
     # Calculation using diff drive dimensions
-    # theta_dot_R = (((omega_targ*W) / 2) + (v/R))
-    # theta_dot_L = ((v*R)/2 - theta_dot_R)
+    # wheel_R = (v_linear + omega_targ * W / 2) / R
+    # wheel_L = (v_linear - omega_targ * W / 2) / R
     
     # Set the wheel velocities
     bot.setVelocity(int(theta_dot_L), int(theta_dot_R))
-    print("L:", theta_dot_L, ", R:", theta_dot_R)
+    print("L:", theta_dot_L, ", R:", theta_dot_R, "theta_diff:", theta_diff, "target_theta:", theta_targ)
     
 
     
